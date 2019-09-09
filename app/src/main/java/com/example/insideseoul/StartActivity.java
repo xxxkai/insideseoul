cf010aedf0c8144e062cf8590944d7566e798ed6
xxxkai <39793208+xxxkai@users.noreply.github.com>
2019-09-09 23:12:48 +0900

    Add Loading Animation & etc
    Add Loading Animation
    Add Test Main Activity layout

--- app/src/main/java/com/example/insideseoul/StartActivity.java
+++ app/src/main/java/com/example/insideseoul/StartActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -12,18 +13,37 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
         Handler timer = new Handler();
 
         timer.postDelayed(new Runnable(){
-
             public void run(){
+                ImageView loading = findViewById(R.id.loading_img);
+                loading.setImageDrawable(getResources().getDrawable(R.drawable.loading_1));
+            }
+        }, 700);
 
+        timer.postDelayed(new Runnable(){
+            public void run(){
+                ImageView loading = findViewById(R.id.loading_img);
+                loading.setImageDrawable(getResources().getDrawable(R.drawable.loading_2));
+            }
+        }, 1400);
+
+        timer.postDelayed(new Runnable(){
+            public void run(){
+                ImageView loading = findViewById(R.id.loading_img);
+                loading.setImageDrawable(getResources().getDrawable(R.drawable.loading_3));
+            }
+        }, 2100);
+
+        timer.postDelayed(new Runnable(){
+            public void run(){
                 //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 Intent intent = new Intent(getApplicationContext(), TestActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 2500);
+
     }
 }
