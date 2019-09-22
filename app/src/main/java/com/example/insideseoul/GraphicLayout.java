3af784149556c5a65dd3d4924eb680fe847e5024
xxxkai <39793208+xxxkai@users.noreply.github.com>
2019-09-23 00:32:39 +0900

    Modify MapView
    -

--- app/src/main/java/com/example/insideseoul/GraphicLayout.java
+++ app/src/main/java/com/example/insideseoul/GraphicLayout.java
@@ -1,13 +1,17 @@
 package com.example.insideseoul;
 
 public class GraphicLayout {
-    static String Names[] = {
+    static String Local_Names[] = {
+            // 북쪽
             "종로구","중구","용산구","성동구","광진구",
             "동대문구","중랑구","성북구","강북구","도봉구",
-            "노원구","은평구","서대문구","마포구","양천구",
-            "강서구","구로구","금천구","영등포구","동작구",
+            "노원구","은평구","서대문구","마포구",
+
+            // 남쪽
+            "양천구", "강서구","구로구","금천구","영등포구","동작구",
             "관악구","서초구","강남구","송파구","강동구"
     };
+
     static int id_list[] = {
             1, 2
     };
@@ -17,13 +21,24 @@ public class GraphicLayout {
     };
 
     public static String getName(int index){
-        return Names[index];
+        return Local_Names[index];
     }
 
     public static int getIndex(String name){
-        for(int i = 0; i < Names.length; i++)
-            if(Names[i].equals(name)) return i;
+        for(int i = 0; i < Local_Names.length; i++)
+            if(Local_Names[i].equals(name)) return i;
         return -1;
+    }
+
+    public static char getDirection(String name){
+        int index = getIndex(name);
+
+        if(index >= 0 && index <= 13)
+            return 'n';
+        else if(index >= 14 && index <= 24)
+            return 's';
+        else
+            return ' ';
     }
 
 }
--- app/src/main/java/com/example/insideseoul/MainActivity.java
+++ app/src/main/java/com/example/insideseoul/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -18,7 +19,10 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     static boolean graphic_mode = false;
@@ -152,10 +156,67 @@ public class MainActivity extends AppCompatActivity {
     public void viewMap1(View v){
         showMsg("서울시 북부 목록을 표시합니다.");
         onlyOneVisible(contents_index.GRAPHIC_VIEW2.getValue());
-        ImageView img[] = new ImageView[3];
-        img[0] = findViewById(R.id.map_dummy1);
-        img[1] = findViewById(R.id.map_dummy2);
+        int[] icon_ids = { 	R.id.local_00, R.id.local_01, R.id.local_02, R.id.local_03, R.id.local_04,
+                            R.id.local_05, R.id.local_06, R.id.local_07, R.id.local_08, R.id.local_09,
+                            R.id.local_10, R.id.local_11, R.id.local_12, R.id.local_13,
+                            /* R.id.local_14,
+                            R.id.local_15, R.id.local_16, R.id.local_17, R.id.local_18, R.id.local_19,
+                            R.id.local_20, R.id.local_21, R.id.local_22, R.id.local_23, R.id.local_24*/
+                            };
+        int[] alert_ids = { 	R.id.local_alert_00, R.id.local_alert_01, R.id.local_alert_02, R.id.local_alert_03, R.id.local_alert_04,
+                                R.id.local_alert_05, R.id.local_alert_06, R.id.local_alert_07, R.id.local_alert_08, R.id.local_alert_09,
+                                R.id.local_alert_10, R.id.local_alert_11, R.id.local_alert_12, R.id.local_alert_13,
+                                /* R.id.local_alert_14,
+                                R.id.local_alert_15, R.id.local_alert_16, R.id.local_alert_17, R.id.local_alert_18, R.id.local_alert_19,
+                                R.id.local_alert_20, R.id.local_alert_21, R.id.local_alert_22, R.id.local_alert_23, R.id.local_alert_24*/
+                             };
+        int[] img_ids = { 	R.id.local_img_00, R.id.local_img_01, R.id.local_img_02, R.id.local_img_03, R.id.local_img_04,
+                            R.id.local_img_05, R.id.local_img_06, R.id.local_img_07, R.id.local_img_08, R.id.local_img_09,
+                            R.id.local_img_10, R.id.local_img_11, R.id.local_img_12, R.id.local_img_13,
+                            /* R.id.local_img_14,
+                            R.id.local_img_15, R.id.local_img_16, R.id.local_img_17, R.id.local_img_18, R.id.local_img_19,
+                            R.id.local_img_20, R.id.local_img_21, R.id.local_img_22, R.id.local_img_23, R.id.local_img_24*/
+        };
+        int[] img_drawables = { 	R.drawable.map_00, R.drawable.map_01, R.drawable.map_02, R.drawable.map_03, R.drawable.map_04,
+                                    R.drawable.map_05, R.drawable.map_06, R.drawable.map_07, R.drawable.map_08, R.drawable.map_09,
+                                    R.drawable.map_10, R.drawable.map_11, R.drawable.map_12, R.drawable.map_13,
+                                    /* R.drawable.map_14,
+                                    R.drawable.map_15, R.drawable.map_16, R.drawable.map_17, R.drawable.map_18, R.drawable.map_19,
+                                    R.drawable.map_20, R.drawable.map_21, R.drawable.map_22, R.drawable.map_23, R.drawable.map_24*/
+        };
+        int[] name_ids = { 	R.id.local_name_00, R.id.local_name_01, R.id.local_name_02, R.id.local_name_03, R.id.local_name_04,
+                            R.id.local_name_05, R.id.local_name_06, R.id.local_name_07, R.id.local_name_08, R.id.local_name_09,
+                            R.id.local_name_10, R.id.local_name_11, R.id.local_name_12, R.id.local_name_13,
+                            /* R.id.local_name_14,
+                            R.id.local_name_15, R.id.local_name_16, R.id.local_name_17, R.id.local_name_18, R.id.local_name_19,
+                            R.id.local_name_20, R.id.local_name_21, R.id.local_name_22, R.id.local_name_23, R.id.local_name_24*/
+        };
 
+        LinearLayout[] icons = new LinearLayout[icon_ids.length];
+        Button[] alerts = new Button[alert_ids.length];
+        ImageView[] map_imgs = new ImageView[img_ids.length];
+        TextView[] names = new TextView[name_ids.length];
+
+        Random rand = new Random();
+        int alert_num = 0;
+        int congestion = 0;
+
+        for(int i = 0; i < icons.length; i++){
+            icons[i] = findViewById(icon_ids[i]);
+            alerts[i] = findViewById(alert_ids[i]);
+            map_imgs[i] = findViewById(img_ids[i]);
+            names[i] = findViewById(name_ids[i]);
+
+            alert_num = rand.nextInt()%5;
+            congestion = rand.nextInt()%3;
+            alert_num = (alert_num<0)?(alert_num*-1):alert_num;
+            congestion = (congestion<0)?((congestion*-1)):(congestion);
+
+            alerts[i].setText(""+alert_num);
+            names[i].setText(GraphicLayout.getName(i));
+            map_imgs[i].setImageDrawable(getResources().getDrawable(img_drawables[i], getApplicationContext().getTheme()));
+            map_imgs[i].setColorFilter(Color.parseColor(GraphicLayout.Colors[congestion]));
+        }
 
     }
 
